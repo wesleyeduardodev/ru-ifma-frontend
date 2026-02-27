ed2bee215056d2842a7cf47bb21daa9e733e4ec6
Wesley Eduardo <wesleyeduardo.dev@gmail.com>
2026-02-27 15:56:32 -0300

    configurando base url da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://ru-ifma-backend-production-6229.up.railway.app';
+//const API_URL = 'http://localhost:8080';
 
 const api = axios.create({
   baseURL: API_URL,
